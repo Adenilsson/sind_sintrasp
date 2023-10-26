5cbbf04cedfd0f7d54175fd96cf0f25b14d6a0e6
Adenilson <adenilson@fairtek.com.br>
2023-10-26 20:18:04 -0300

    26-10-23

--- dao/UserDao.php
+++ dao/UserDao.php
@@ -1,70 +1,38 @@
-<?php 
-require("Conexao.php");
-class UserDao{
+<?php
 
+use sys4soft\Database;
 
+define('MYSQL_CONFIG', [
+    'host' => 'localhost',
+    'database' => 'bd_stock',
+    'username' => 'root',
+    'password' => '',
+]);
 
+require_once('Database.php');
 
-    public static $instance;
+$db = new Database(MYSQL_CONFIG);
 
-   public function __construct() {
-      //
-    }
+// SELECT
+$results = $db->execute_query("SELECT * FROM stock limit 100");
 
-      public static function getInstance() {
-        if (!isset(self::$instance))
-            self::$instance = new UserDao();
+// INSERT
+$params = [
+    ':produto' => 'produto um',
+    ':quantidade' => 100
+];
+$results = $db->execute_non_query("INSERT INTO stock(produto, quantidade) VALUES(:produto, :quantidade)", $params);
 
-        return self::$instance;
-     }
-    public function cadUser(User $user){
-        try{
-            $sql = "INSERT INTO tb_user(
-                nome,
-                user_name,
-                matricula,
-                cpf,
-                email,
-                tipo,
-                tb_perfil_id,
-                created,
-                ativo,
-                updated,
-                tb_funcao_id
-            )VALUE(
-                :nome,
-                :user_name,
-                :matricula,
-                :cpf,
-                :email,
-                :tipo,
-                :tb_perfil_id,
-                :created,
-                :ativo,
-                :updated,
-                :tb_funcao_id
-            )";
+// UPDATE
+$params = [
+    ':id' => 5,
+    ':produto' => 'produto atualizado'
+];
+$results = $db->execute_non_query("UPDATE stock SET produto = :produto WHERE id = :id", $params);
 
- 
-            $p_sql = Conexao::getInstance()->prepare($sql);
-            $p_sql->bindValue(":nome",$user->getNome());
-            $p_sql->bindValue(":user_name",$user->getUserName());
-            $p_sql->bindValue(":matricula",$user->getMatricula());
-            $p_sql->bindValue(":cpf",$user->getCpf());
-            $p_sql->bindValue(":email",$user->getEmail());
-            $p_sql->bindValue("tipo", $user->getTipo());
-            $p_sql->bindValue(":tb_perfil_id",$user->getPerfilId());
-            $p_sql->bindValue(":created",$user->getCreated());
-            $p_sql->bindValue(":ativo",$user->getAtivo());
-            $p_sql->bindValue(":updated",$user->getUpdated());
-            $p_sql->bindValue(":tb_funcao_id", $user->getFuncaoId());
-         
-            var_dump($p_sql);
-            return $p_sql->execute();
-        }catch(Exception $e){
-            print_r("Ocorreu um erro ao cadastra o usuario.".$e->getMessage());
-            /*GeraLog::getInstance()->inserirLog("Erro: Código: "
-           . $e->getCode() . " Mensagem: " . $e->getMessage());*/
-        }
-    }   
-};
+// DELETE
+$params = [
+    ':id' => 5
+];
+$results = $db->execute_non_query("DELETE FROM stock WHERE id = 5");
+
